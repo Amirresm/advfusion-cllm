623fd09bf243826c2f7da7d329668d778871c708
Amirreza Esmaeili <amirreza.77.esmaeili@gmail.com>
2025-09-17 13:55:04 -0700

    add compacter advfusion

---
+++ jobs/dra/qwen2.5coder3b_ct/advfc_julia.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=8:00:00
+#SBATCH --account=rrg-fard
+#SBATCH --mem-per-cpu=16000M
+#SBATCH --gpus-per-node=1
+#SBATCH --output=O-%x.%j.out
+
+echo "TMP DIR: ${SLURM_TMPDIR}"
+echo "PWD: ${PWD}"
+echo "HOST: $(hostname)"
+echo "ls, ls .., ls ../.."
+ls
+ls ..
+ls ../..
+echo "End of debug info"
+
+PROJECT_ROOT="/home/amirresm/files/advfusion"
+cd "$PROJECT_ROOT"
+source "$PROJECT_ROOT/jobs/dra/_setup.sh"
+
+echo "Starting job on '$MACHINE' at $(date) in project root: $PROJECT_ROOT"
+
+lang="julia"
+
+OUTPUT_DIR="/scratch/amirresm/outputs/advfusion/qwen2.5coder3b_ct/advfc_${lang}"
+mkdir -p "$OUTPUT_DIR"
+rm "$OUTPUT_DIR"/job.log || true
+exec > >(tee -a "$OUTPUT_DIR/job.log") 2>&1
+pip freeze >"$OUTPUT_DIR/requirements.txt"
+
+model_path="$STORAGE_ROOT/models/Qwen/Qwen2.5-Coder-3B"
+ds_path="$STORAGE_ROOT/data/ct_dataset/${lang}"
+
+target_adapter_path="/scratch/amirresm/outputs/advfusion/qwen2.5coder3b_ct/compacter_${lang}"
+
+adapter_path_list=(
+	"/scratch/amirresm/outputs/advfusion/qwen2.5coder3b_ct/compacter_julia"
+	"/scratch/amirresm/outputs/advfusion/qwen2.5coder3b_ct/compacter_ruby"
+	"/scratch/amirresm/outputs/advfusion/qwen2.5coder3b_ct/compacter_scala"
+	"/scratch/amirresm/outputs/advfusion/qwen2.5coder3b_ct/compacter_swift"
+)
+
+benchmark_dataset_name_or_path="$STORAGE_ROOT/data/ct_bench_dataset/ct_bench_dataset_all_${lang}.jsonl"
+
+python -m scripts.train_advf \
+	--model_name_or_path "${model_path}" \
+	--q "4bit" \
+	--adapter_path_list "${adapter_path_list[@]}" \
+	--target_adapter_path "$target_adapter_path" \
+	--dataset_name_or_path "${ds_path}" \
+	--train_file train.jsonl \
+	--validation_file valid.jsonl \
+	--test_file test.jsonl \
+	--max_train_samples 9999999 \
+	--max_eval_samples 9999999 \
+	--max_test_samples 9999999 \
+	--chunk_size 0 \
+	--train_text_max_length 4096 \
+	--train_target_max_length 4096 \
+	--train_max_length 8192 \
+	--epochs 2 \
+	--do_train \
+	--train_completions_only False \
+	--train_batch_size 4 \
+	--gradient_accumulation_steps 1 \
+	--learning_rate 1e-4 \
+	--do_eval \
+	--eval_batch_size 4 \
+	--logging_steps 0.05 \
+	--eval_steps 0.2 \
+	--valid_text_max_length 2048 \
+	--valid_target_max_length 2048 \
+	--gen_pre_train_max_samples 32 \
+	--gen_batch_size 32 \
+	--test_text_max_length 4096 \
+	--test_target_max_length 2048 \
+	--benchmark_dataset_name_or_path "${benchmark_dataset_name_or_path}" \
+	--benchmark_do_sample 1 \
+	--benchmark_temperature 0.3 \
+	--benchmark_top_p 0.9 \
+	--benchmark_top_k 50 \
+	--benchmark_n_per_sample 10 \
+	--output_dir "${OUTPUT_DIR}"
--- src/advfusion/advfusion_utils.py
+++ src/advfusion/advfusion_utils.py
@@ -1,29 +1,81 @@
+def detect_adapter_type(adapter_module, adapter_name):
+    """
+    Detect the type of adapter module between seq_bn and compacter.
+    """
+    if hasattr(adapter_module.adapters[adapter_name].adapter_down[0], "W_left"):
+        return "compacter"
+    return "seq_bn"
+
+
 def get_module_modifier(device, adapter_name, freeze=False, zero=False):
+
     def processor(_, module):
         if hasattr(module, "adapters") and adapter_name in module.adapters:
+            adapter_type = detect_adapter_type(module, adapter_name)
             if freeze:
-                module.adapters[adapter_name].adapter_down[
-                    0
-                ].weight.requires_grad = False
-                module.adapters[adapter_name].adapter_down[
-                    0
-                ].bias.requires_grad = False
+                if adapter_type == "compacter":
+                    module.adapters[adapter_name].adapter_down[
+                        0
+                    ].W_left.requires_grad = False
+                    module.adapters[adapter_name].adapter_down[
+                        0
+                    ].W_right.requires_grad = False
+                    module.adapters[adapter_name].adapter_down[
+                        0
+                    ].b.requires_grad = False
+                    module.adapters[
+                        adapter_name
+                    ].adapter_up.W_left.requires_grad = False
+                    module.adapters[
+                        adapter_name
+                    ].adapter_up.W_right.requires_grad = False
+                    module.adapters[adapter_name].adapter_up.b.requires_grad = (
+                        False
+                    )
+                else:
+                    module.adapters[adapter_name].adapter_down[
+                        0
+                    ].weight.requires_grad = False
+                    module.adapters[adapter_name].adapter_down[
+                        0
+                    ].bias.requires_grad = False
 
-                module.adapters[
-                    adapter_name
-                ].adapter_up.weight.requires_grad = False
-                module.adapters[adapter_name].adapter_up.bias.requires_grad = (
-                    False
-                )
+                    module.adapters[
+                        adapter_name
+                    ].adapter_up.weight.requires_grad = False
+                    module.adapters[
+                        adapter_name
+                    ].adapter_up.bias.requires_grad = False
 
             if zero:
-                module.adapters[adapter_name].adapter_down[0].weight.data.fill_(
-                    0
-                )
-                module.adapters[adapter_name].adapter_down[0].bias.data.fill_(0)
-
-                module.adapters[adapter_name].adapter_up.weight.data.fill_(0)
-                module.adapters[adapter_name].adapter_up.bias.data.fill_(0)
+                if adapter_type == "compacter":
+                    module.adapters[adapter_name].adapter_down[
+                        0
+                    ].W_left.data.fill_(0)
+                    module.adapters[adapter_name].adapter_down[
+                        0
+                    ].W_right.data.fill_(0)
+                    module.adapters[adapter_name].adapter_down[0].b.data.fill_(
+                        0
+                    )
+                    module.adapters[adapter_name].adapter_up.W_left.data.fill_(
+                        0
+                    )
+                    module.adapters[adapter_name].adapter_up.W_right.data.fill_(
+                        0
+                    )
+                    module.adapters[adapter_name].adapter_up.b.data.fill_(0)
+                else:
+                    module.adapters[adapter_name].adapter_down[
+                        0
+                    ].weight.data.fill_(0)
+                    module.adapters[adapter_name].adapter_down[
+                        0
+                    ].bias.data.fill_(0)
+                    module.adapters[adapter_name].adapter_up.weight.data.fill_(
+                        0
+                    )
+                    module.adapters[adapter_name].adapter_up.bias.data.fill_(0)
 
     return processor
 
